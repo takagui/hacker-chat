bf51f1a3dddff6f80949033c0a882288698759c6
Guilherme Takahashi <taka.gui@outlook.com>
2021-03-23 23:46:35 -0300

    create interface using blessed and init chat project

---
+++ client/src/terminalController.js
@@ -0,0 +1,123 @@
+import { ComponentsBuilder } from './components.js';
+import { constants } from './constants.js';
+
+export class TerminalController {
+  #usersColors = new Map();
+
+  constructor() {}
+
+  #pickColor() {
+    return '#' + ((1 << 24) * Math.random() | 0).toString(16) + '-fg';
+  }
+
+  #getUserColor(userName) {
+    if (this.#usersColors.has(userName))
+      return this.#usersColors.get(userName);
+
+    const color = this.#pickColor();
+
+    this.#usersColors.set(userName, color);
+
+    return color;
+  }
+
+  #onInputReceived(eventEmitter) {
+    return function () {
+      const message = this.getValue();
+
+      console.log(message);
+
+      this.clearValue();
+    }
+  }
+
+  #onMessageReceived({ screen, chat }) {
+    return msg => {
+      const { userName, message } = msg;
+      const color = this.#getUserColor(userName);
+
+      chat.addItem(`{${color}}{bold}${userName}{/}: ${message}`);
+
+      screen.render();
+    }
+  }
+
+  #onStatusChanged({ screen, status }) {
+    // [ 'takagui' 'nakaharen' ]
+
+    return users => {
+      const { content } = status.items.shift();
+
+      status.clearItems();
+      status.addItem(content);
+
+      users.forEach(userName => {
+        const color = this.#getUserColor(userName);
+        status.addItem(`{${color}}{bold}${userName}{/}`);
+      });
+
+      screen.render();
+    }
+  }
+
+  #onLogChanged({ screen, activityLog }) {
+    // takagui join
+    // takagui left
+
+    return msg => {
+      const [userName] = msg.split(/\s/);
+      const color = this.#getUserColor(userName);
+
+      activityLog.addItem(`{${color}}{bold}${msg.toString()}{/}`);
+
+      screen.render();
+    }
+  }
+
+  #registerEvents(eventEmitter, components) {
+    eventEmitter.on(
+      constants.events.app.MESSAGE_RECEIVED,
+      this.#onMessageReceived(components)
+    );
+
+    eventEmitter.on(
+      constants.events.app.STATUS_UPDATED,
+      this.#onStatusChanged(components)
+    );
+
+    eventEmitter.on(
+      constants.events.app.ACTIVITY_UPDATED,
+      this.#onLogChanged(components)
+    );
+  }
+
+  async initializeTable(eventEmitter) {
+    const components = new ComponentsBuilder()
+      .setScreen({ title: 'HackerChat - Guilherme Takahashi' })
+      .setLayoutComponent()
+      .setInputComponent(this.#onInputReceived(eventEmitter))
+      .setChatComponent()
+      .setStatusComponent()
+      .setActivityLogComponent()
+      .build();
+
+    this.#registerEvents(eventEmitter, components);
+
+    components.input.focus();
+    components.screen.render();
+
+    setInterval(() => {
+      const users = ['takagui'];
+      eventEmitter.emit(constants.events.app.STATUS_UPDATED, users);
+      users.push('nakaharen');
+      eventEmitter.emit(constants.events.app.STATUS_UPDATED, users);
+
+      eventEmitter.emit(constants.events.app.MESSAGE_RECEIVED, {message: 'hey', userName: 'takagui'});
+      eventEmitter.emit(constants.events.app.MESSAGE_RECEIVED, {message: 'hey', userName: 'nakaharen'});
+      eventEmitter.emit(constants.events.app.ACTIVITY_UPDATED, 'takagui join');
+      eventEmitter.emit(constants.events.app.ACTIVITY_UPDATED, 'nakaharen join');
+      eventEmitter.emit(constants.events.app.ACTIVITY_UPDATED, 'takagui left');
+      eventEmitter.emit(constants.events.app.ACTIVITY_UPDATED, 'nakaharen left');
+    }, 2000);
+  }
+}
